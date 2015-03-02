73870b77165cdaab2a332ece8e5dac2a161d6789
deniznida <denizunat@hotmail.com>
2015-03-02 12:37:49 -0500

    done

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,15 @@
 # Enter your procedural solution here!
+def collect_multiples(limit)
+  (1...limit).select do |num|
+    (num % 3 == 0 || num % 5 == 0)
+  end
+end
+
+
+def sum_multiples(limit)
+  sum = 0
+  collect_multiples(limit).each do |num|
+    sum += num
+  end
+  sum
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,28 @@
 # Enter your object-oriented solution here!
+class Multiples
+  attr_accessor :limit, :start, :multiples
+
+  def initialize(limit, start=1)
+    @limit = limit
+    @start = start
+    @multiples = multiples
+  end
+
+  def collect_multiples
+    self.multiples = (self.start...self.limit).select do |num|
+      (num % 3 == 0 || num % 5 == 0)
+    end
+  end
+
+  def sum_multiples
+    collect_multiples
+    sum = 0
+    self.multiples.each do |num|
+      sum += num
+    end
+    sum
+  end
+end
+
+class ArgumentError < StandardError
+end
